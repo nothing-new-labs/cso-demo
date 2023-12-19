83be6c062ee9afe5aff7fad03606c8fe7c182cab
linbo <linbo.lin@qq.com>
2023-12-19 23:46:36 +0800

    Refine code

--- src/rule/exploration/filter_2_index_scan.rs
+++ src/rule/exploration/filter_2_index_scan.rs
@@ -50,33 +50,38 @@ impl Rule<Demo> for Filter2IndexScan {
             .logical_op()
             .downcast_ref::<LogicalScan>()
             .expect("LogicalScan expected!");
+
         let table_desc = logical_scan.table_desc();
-
         let md_accessor = context.md_accessor();
-
         let relation_md = md_accessor
             .retrieve_metadata(&table_desc.md_id())
             .expect("Relation metadata missed!");
-
         let relation_md = relation_md.downcast_ref::<RelationMetadata>().unwrap();
-        let index_count = relation_md.index_count();
 
         let mut new_plans = vec![];
-        for i in 0..index_count {
+        for i in 0..relation_md.index_count() {
             let index_mdid = relation_md.index_mdid(i);
             let index_md = md_accessor
                 .retrieve_metadata(&index_mdid)
                 .expect("Index metadata missed!");
             let index_md = index_md.downcast_ref::<IndexMd>().unwrap();
-            if index_matched(index_md, logical_filter.predicate().as_ref()) {
+            let predicate = logical_filter.predicate();
+            let (applicable, residual_predicate) = index_matched(index_md, predicate.as_ref());
+            if applicable {
                 let logical_index_scan = LogicalIndexScan::new(
                     table_desc.clone(),
                     index_md,
                     logical_scan.output_columns().to_vec(),
-                    logical_filter.predicate().clone(),
+                    predicate.clone(),
                 );
-                let plan = Plan::new(Operator::Logical(Rc::new(logical_index_scan)), vec![], None);
-                new_plans.push(plan);
+                let mut new_plan = Plan::new(Operator::Logical(Rc::new(logical_index_scan)), vec![], None);
+
+                if let Some(residual_predicate) = residual_predicate {
+                    let new_logical_filter = LogicalFilter::new(residual_predicate);
+                    new_plan = Plan::new(Operator::Logical(Rc::new(new_logical_filter)), vec![new_plan], None);
+                }
+
+                new_plans.push(new_plan);
             }
         }
         new_plans
@@ -87,6 +92,6 @@ impl Rule<Demo> for Filter2IndexScan {
     }
 }
 
-fn index_matched(_index_md: &IndexMd, _predicate: &dyn ScalarExpression) -> bool {
-    true
+fn index_matched(_index_md: &IndexMd, _predicate: &dyn ScalarExpression) -> (bool, Option<Rc<dyn ScalarExpression>>) {
+    (true, None)
 }
