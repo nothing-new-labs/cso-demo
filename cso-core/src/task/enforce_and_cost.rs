97b685cee7c1b00a271101dad9b08cc15ac79791
linbo <linbo.lin@qq.com>
2024-04-12 22:04:40 +0800

    Refine code: EnforceAndCostTask::execute() (#27)

--- cso-core/src/task/enforce_and_cost.rs
+++ cso-core/src/task/enforce_and_cost.rs
@@ -1,4 +1,4 @@
-use crate::memo::{GroupPlanRef, GroupRef};
+use crate::memo::{GroupPlanRef, GroupRef, LowestCostPlan};
 use crate::property::PhysicalProperties;
 use crate::task::{Task, TaskRunner};
 use crate::{OptimizeGroupTask, OptimizerContext, OptimizerType};
@@ -44,31 +44,31 @@ impl<T: OptimizerType> EnforceAndCostTask<T> {
      * 1. make require property for children base of current operator
      * 2. try to optimize child group and get best (Cost, GroupPlan) pair of every children
      * 3. once we get all output property of one candidate loop, derive output property base of current operator
-     * 4. if output property does not satisfied require property, add enforcers and submit (Cost, GroupPlan) pair
+     * 4. if output property does not satisfy require property, add enforcers and submit (Cost, GroupPlan) pair
      */
     pub(super) fn execute(mut self, task_runner: &mut TaskRunner<T>, optimizer_ctx: &mut OptimizerContext<T>) {
-        // 1. according to current operator create new requestPropList for children
+        // get required properties for children
         let child_reqd_props_list = self.init_child_required_props_list();
 
         for (index, child_reqd_props) in child_reqd_props_list.iter().skip(self.prev_index).enumerate() {
-            let mut total_cost = self.plan.borrow().compute_cost();
+            let mut cost = self.plan.borrow().compute_cost();
+            let mut child_output_props = Vec::with_capacity(child_reqd_props.len());
 
-            let mut child_output_props = vec![];
-            for child_index in 0..child_reqd_props.len() {
+            for (child_index, child_reqd_prop) in child_reqd_props.iter().enumerate() {
                 let curr_child_ref = self.child(child_index);
                 let curr_child = curr_child_ref.borrow();
-                let child_reqd_prop = &child_reqd_props[child_index];
 
-                // 2. optimize children groups using requestPropList
+                // check whether the current child group is already optimized for the current child_reqd_prop,
+                // if we have optimized current child group, we can get the best (Cost, GroupPlan).
+                // otherwise, we need to optimize current child group first.
                 match curr_child.lowest_cost_plans().get(child_reqd_prop) {
-                    Some((cost, plan)) => {
+                    Some((child_cost, plan)) => {
                         let output_prop = plan.borrow().get_output_prop(child_reqd_prop).clone();
                         child_output_props.push(output_prop);
-                        total_cost += *cost;
+                        cost += *child_cost;
                     }
                     None => {
                         self.prev_index = index;
-                        // 3. get output property of child groups and add enforcer to cost and plan pair
                         task_runner.push_task(self.clone());
                         let task = OptimizeGroupTask::new(curr_child_ref.clone(), child_reqd_prop.clone());
                         task_runner.push_task(task);
@@ -77,41 +77,67 @@ impl<T: OptimizerType> EnforceAndCostTask<T> {
                 }
             }
 
-            // 4. now assume we have optimize child groups for child_required_props and get one best cost and plan pairs
-            // and we want to compare require_prop and output_prop derived by child output props
-            // if do not satisfy, add enforcer
-            let mut curr_plan = self.plan.borrow_mut();
-            let output_prop = curr_plan.derive_output_properties(&child_output_props);
-            curr_plan.update_require_to_output_map(&output_prop, &output_prop);
+            // successfully optimize all child group, and we can compute the output property for current operator.
+            let output_prop = self.derive_output_props(&child_output_props);
+            self.submit_best_plan(&output_prop, (cost, self.plan.clone()), child_reqd_props.clone());
 
-            {
-                let curr_group_ref = curr_plan.group();
-                let mut curr_group = curr_group_ref.borrow_mut();
-                curr_group.update_cost_plan(&output_prop, &self.plan, total_cost);
-                curr_group.update_child_required_props(&output_prop, child_reqd_props.clone(), total_cost);
+            // enforce property if output_prop doesn't satisfy self.required_prop
+            let enforcer = self.add_enforcer(&output_prop, &self.required_prop, optimizer_ctx);
+            match enforcer {
+                Some(enforcer) => {
+                    cost += enforcer.borrow().compute_cost();
+                    self.submit_best_plan(&self.required_prop, (cost, enforcer), vec![output_prop])
+                }
+                None => self.submit_best_plan(&self.required_prop, (cost, self.plan.clone()), child_reqd_props.clone()),
             }
+        }
+    }
 
-            if !output_prop.satisfy(&self.required_prop) {
-                let curr_group = curr_plan.group();
-                let enforer = self.required_prop.make_enforcer(curr_group.clone());
-                let enforer = optimizer_ctx.memo.insert_group_plan(enforer, Some(curr_group.clone()));
-                enforer
-                    .borrow_mut()
-                    .update_require_to_output_map(&self.required_prop, &self.required_prop);
-                let enforer_cost = enforer.borrow().compute_cost();
-                total_cost += enforer_cost;
+    fn derive_output_props(&self, child_output_props: &[Rc<PhysicalProperties<T>>]) -> Rc<PhysicalProperties<T>> {
+        let curr_plan = self.plan.borrow();
+        curr_plan.derive_output_properties(&child_output_props)
+    }
 
-                let mut curr_group = curr_group.borrow_mut();
-                curr_group.update_cost_plan(&self.required_prop, &enforer, total_cost);
-                curr_group.update_child_required_props(&self.required_prop, vec![output_prop], total_cost);
-            } else {
-                curr_plan.update_require_to_output_map(&self.required_prop, &self.required_prop);
+    fn submit_best_plan(
+        &self,
+        required_prop: &Rc<PhysicalProperties<T>>,
+        lowest_cost_plan: LowestCostPlan<T>,
+        child_reqd_props: Vec<Rc<PhysicalProperties<T>>>,
+    ) {
+        let (cost, best_plan) = lowest_cost_plan;
 
-                let curr_group_ref = curr_plan.group();
-                let mut curr_group = curr_group_ref.borrow_mut();
-                curr_group.update_cost_plan(&self.required_prop, &self.plan, total_cost);
-                curr_group.update_child_required_props(&self.required_prop, child_reqd_props.clone(), total_cost);
-            }
+        // The passed-in best_plan might be self.plan, in which case there would be an issue with duplicate borrows.
+        // To address this problem, we explicitly add `{}` to control the lifetime of the borrow.
+        {
+            let curr_plan = self.plan.borrow();
+            let curr_group = curr_plan.group();
+            let mut curr_group = curr_group.borrow_mut();
+
+            curr_group.update_cost_plan(required_prop, (cost, best_plan.clone()));
+            curr_group.update_child_required_props(required_prop, cost, child_reqd_props);
+        }
+
+        {
+            let mut best_plan = best_plan.borrow_mut();
+            best_plan.update_require_to_output_map(required_prop, required_prop);
+        }
+    }
+
+    fn add_enforcer(
+        &self,
+        output_prop: &Rc<PhysicalProperties<T>>,
+        required_prop: &Rc<PhysicalProperties<T>>,
+        optimizer_ctx: &mut OptimizerContext<T>,
+    ) -> Option<GroupPlanRef<T>> {
+        let curr_plan = self.plan.borrow();
+        let curr_group = curr_plan.group();
+
+        if !output_prop.satisfy(required_prop) {
+            let enforcer = required_prop.make_enforcer(curr_group.clone());
+            let enforcer = optimizer_ctx.memo.insert_group_plan(enforcer, Some(curr_group));
+            Some(enforcer)
+        } else {
+            None
         }
     }
 }
